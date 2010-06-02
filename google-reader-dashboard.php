915479a022911f239f539492f8f506db8de7fa81
ericmann <ericmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-02 13:57:01 +0000

    Tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/google-reader-dashboard/tags/1.0.0@248159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google-reader-dashboard.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Google Reader Dashboard
+Plugin URI: http://www.jumping-duck.com/wordpress/
+Description: Adds a Google Reader dashboard widget to the WordPress admin screen.
+Version: 1.0.0
+Author: Eric Mann
+Author URI: http://www.eamann.com
+License: GPL2
+*/
+
+/*  Copyright 2010  Eric Mann  (email : eric@eamann.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Define global variables and constants
+global $grd_reporter;
+if( ! defined( 'GRD_VER' ))
+	define( 'GRD_VER', '1.0.0' );
+if( ! defined( 'GRD_URL' ) )
+	define( 'GRD_URL', get_bloginfo('url') . '/wp-content/plugins/google-reader-dashboard' );
+if( ! defined( 'GRD_INC_URL' ) )
+	define( 'GRD_INC_URL', GRD_URL . '/includes');
+if( ! defined( 'GRD_BASE_URL' ) )
+	define( 'GRD_BASE_URL', dirname(__FILE__) );	
+if( ! defined( 'GRD_BASE_INC_URL' ) )
+	define( 'GRD_BASE_INC_URL', GRD_BASE_URL . '/includes');
+
+/* Activate Error Reporter */
+//include_once( GRD_BASE_INC_URL . '/elliot.class.php' );
+if(class_exists('ElliotClientV1')) $grd_reporter = new ElliotClientV1('http://jumping-duck.com/xmlrpc.php', 'Google Reader Dashboard', GRD_VER);
+
+$installed=get_option('GRD_Installed_Version');
+
+if(!$installed) {
+	update_option('GRD_Installed_Version', '1.0.0');
+	if(class_exists('ElliotClientV1')) $grd_reporter->dispatch('New Installation');
+}
+
+/*
+ * Sets admin warnings regarding required PHP version.
+ */
+function _grd_php_warning() {
+	$data = get_plugin_data(__FILE__);
+	
+	echo '<div class="error"><p><strong>' . __('Warning:') . '</strong> '
+		. sprintf(__('The active plugin %s is not compatible with your installed PHP version.') .'</p><p>',
+			'&laquo;' . $data['Name'] . ' ' . $data['Version'] . '&raquo;')
+		. sprintf(__('%s is required for this plugin.'), 'PHP 5 ');
+	echo '</p></div>';
+}
+
+// START PROCEDURE
+
+// Check required PHP version.
+if ( version_compare(PHP_VERSION, '5.0', '<')) {
+	add_action('admin_notices', '_grd_php_warning');
+} else {
+	include_once ( GRD_BASE_INC_URL . '/core.class.php' );
+	$greader = new GRDplugin();
+	$greader->load();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Google Reader Dashboard ===
+Contributors: ericmann
+Donate link: http://jumping-duck.com/wordpress
+Tags: google reader, feed
+Requires at least: 2.9
+Tested up to: 3.0
+Stable tag: 1.0.0
+
+Adds a feed of your latest unread items from Google Reader.
+
+== Description ==
+
+NOTICE: THIS PLUG-IN REQUIRES PHP5!!!
+
+Add your Google Reader feed to the WordPress admin dashboard.
+
+== Installation ==
+
+1. Upload the `google-reader-dashboard` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Login to the widget with your Google username (gmail address) and password.
+
+== Frequently Asked Questions ==
+
+= Are you stealing my Gmail password? =
+
+No.  For future use, your account information is stored in a cookie on your local machine.  This way you don't have to log in again for a while, but we never send this information anywhere else.  For your protection, the cookie will automatically expire after 7 days.
+
+= What if I see a bug? =
+
+If you see a bug or have a feature request, please make a comment in our bug tracking system at http://bt.jumping-duck.com/
+
+== Screenshots ==
+
+1. This screenshot shows how the widget renders amidst other feed readers and admin page widgets.
+
+== Changelog ==
+
+= 1.0.0 =
+* First Release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+First release
